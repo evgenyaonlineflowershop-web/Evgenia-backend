dbad248dd05d15e7148c4886e2729bf5d654138d
evgenyaonlineflowershop-web <evgenyaonlineflowershop@gmail.com>
2026-06-13 01:32:54 +0500

    fix upload

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,4 @@
 module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: "strapi-provider-upload-supabase",
-      providerOptions: {
-        apiUrl: env("SUPABASE_API_URL"),
-        apiKey: env("SUPABASE_API_KEY"),
-        bucket: env("SUPABASE_BUCKET", "evgenia-media"), // Имя бакета из Шага 1
-        directory: "uploads",
-        options: {},
-      },
-    },
-  },
   email: {
     config: {
       provider: "nodemailer",
@@ -20,5 +8,7 @@ module.exports = ({ env }) => ({
         ignoreTLS: true,
       },
     },
+    responsiveDimensions: false,
   },
+
 });
